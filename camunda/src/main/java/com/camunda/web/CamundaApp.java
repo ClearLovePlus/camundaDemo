1da11ae99fc746342c779b4f93760862ea5fb885
chenhao <chenhao3@tebon.com.cn>
2021-10-22 17:16:41 +0800

    初始化camunda流程平台

---
+++ camunda/src/main/java/com/camunda/web/CamundaApp.java
@@ -0,0 +1,17 @@
+package com.camunda.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: chenhao
+ * @date: 2021-9-28 9:13
+ */
+@SpringBootApplication
+public class CamundaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaApp.class,args);
+
+    }
+}
